6fb7c549603e986d8b280a6265e2fe3e7609f299
salmanja <71104867+salmanja@users.noreply.github.com>
2025-06-13 13:04:07 -0400

    generated components and angular material for modal setup

---
+++ src/app/Components/Form/assets-form/assets-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assets-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './assets-form.component.html',
+  styleUrl: './assets-form.component.scss'
+})
+export class AssetsFormComponent {
+
+}
